6d9784bb5c924681289564e6a7fc6fba9f433b0e
Rutvik Barot <rutvikbarot11@gmail.com>
2024-10-12 13:14:15 +0530

    first commit

---
+++ app/+html.jsx
@@ -0,0 +1,37 @@
+import { ScrollViewStyleReset } from "expo-router/html";
+
+/**
+ * This file is web-only and used to configure the root HTML for every web page during static rendering.
+ * The contents of this function only run in Node.js environments and do not have access to the DOM or browser APIs.
+ */
+export default function Root({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        {/* Disable body scrolling on web to make ScrollView components work closer to how they do on native */}
+        <ScrollViewStyleReset />
+
+        {/* Using raw CSS styles to ensure the background color never flickers in dark mode */}
+        <style dangerouslySetInnerHTML={{ __html: responsiveBackground }} />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
+
+const responsiveBackground = `
+body {
+  background-color: #fff;
+}
+@media (prefers-color-scheme: dark) {
+  body {
+    background-color: #000;
+  }
+}`;
